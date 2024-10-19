c167e8988ac700fe8838784faeefa1d27b16cd53
thuong204 <thuongtc.22it@vku.udn.vn>
2024-10-19 15:44:32 +0700

    fix

--- routes/client/user.route.js
+++ routes/client/user.route.js
@@ -32,7 +32,7 @@ router.get('/oauth2/redirect/google', passport.authenticate('google', {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/user/oauth2/redirect/google',
+    callbackURL: '/user/oauth2/redirect/google',
     scope: ['profile', 'email'],
   }, async (accessToken, refreshToken, profile, cb) => {
     try {
